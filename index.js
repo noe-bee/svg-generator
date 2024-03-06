ce542483f55657d6390a1bebf26f39c61afe0d14
The Other Promise <150410378+noe-bee@users.noreply.github.com>
2024-03-06 15:00:12 -0800

    finished functionality and updated readme

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { Shape, Triangle, Circle, Square } = require("./lib/shapes");
 
+//questions
 const questions = [
   {
     type: "input",
@@ -47,11 +48,31 @@ function init() {
       selectedShape = new Square();
     }
     selectedShape.setColor(shapeColor);
-    const svgElement = `<svg height="200" width="300" xmlns="http://www.w3.org/2000/svg">
+
+    //each const here is for perfect middle alignment of text depending on shape picked
+    const svgCircle = `<svg height="200" width="300" xmlns="http://www.w3.org/2000/svg">
       ${selectedShape.render()}
       <text x="150" y="115" font-size="50" text-anchor="middle" fill="${textColor}">${text}</text>!
     </svg>`;
-    writeToFile("logo.svg", svgElement);
+
+    const svgSquare = `<svg height="200" width="300" xmlns="http://www.w3.org/2000/svg">
+      ${selectedShape.render()}
+      <text x="110" y="115" font-size="50" text-anchor="middle" fill="${textColor}">${text}</text>!
+    </svg>`;
+
+    const svgTriange = `<svg height="200" width="300" xmlns="http://www.w3.org/2000/svg">
+      ${selectedShape.render()}
+      <text x="150" y="130" font-size="50" text-anchor="middle" fill="${textColor}">${text}</text>!
+    </svg>`;
+
+    //if else statement will generate the respective shape chosen
+    if (shape === 'Square') {
+      writeToFile("logo.svg", svgSquare);
+    } else if (shape === 'Circle'){
+      writeToFile("logo.svg", svgCircle);
+    } else{
+      writeToFile("logo.svg", svgTriange);
+    }
   });
 }
 
